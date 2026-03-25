be65fc98c78753a2c9d6b295e373027c2ee1055a
neelxie <kidricederek@gmail.com>
2026-03-25 16:31:17 +0300

    Add auth controller

---
+++ backend/app/Modules/Auth/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Modules\Auth\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login',    [AuthController::class, 'login']);
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('me',      [AuthController::class, 'me']);
+    });
+});
