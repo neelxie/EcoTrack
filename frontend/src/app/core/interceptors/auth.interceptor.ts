c6ab9977965e1d307bc2bb8501cabaee6061a766
neelxie <kidricederek@gmail.com>
2026-04-01 21:07:47 +0300

    Add auth interceptor

---
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,22 @@
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.services';
+import { Router } from '@angular/router';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  const token = auth.token();
+
+  const cloned = token
+    ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
+    : req;
+
+  return next(cloned).pipe(
+    catchError((err: HttpErrorResponse) => {
+      if (err.status === 401) router.navigate(['/auth/login']);
+      return throwError(() => err);
+    }),
+  );
+};
