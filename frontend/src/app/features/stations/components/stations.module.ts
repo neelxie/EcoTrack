079058a3ae99e56526b9cc6fdeed5b1bb30a8f27
neelxie <kidricederek@gmail.com>
2026-04-02 22:06:06 +0300

    Add provision for station finalizing

---
+++ frontend/src/app/features/stations/components/stations.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { StationsRoutingModule } from './stations-routing.module';
+
+@NgModule({
+  imports: [StationsRoutingModule],
+})
+export class StationsModule {}
