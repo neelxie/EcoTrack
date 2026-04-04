fb199498089d5662289dc7d355969ed9590e7558
neelxie <kidricederek@gmail.com>
2026-04-04 20:00:12 +0300

    add alerts module

---
+++ frontend/src/app/features/alerts/components/alerts.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { AlertsRoutingModule } from './alerts-routing.module';
+
+@NgModule({
+  imports: [AlertsRoutingModule],
+})
+export class AlertsModule {}
